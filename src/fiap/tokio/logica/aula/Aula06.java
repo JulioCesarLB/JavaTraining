722527f395ca21f65dd0bcf8b1c6fd39b6b9b555
JulioCesarLB <julio.lifeintech@gmail.com>
2022-04-20 15:10:06 -0300

    Test

---
+++ src/fiap/tokio/logica/aula/Aula06.java
@@ -0,0 +1,10 @@
+package fiap.tokio.logica.aula;
+
+public class Aula06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world, test");
+	}
+
+}
