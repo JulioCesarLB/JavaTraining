9404a5013b7730aaed3a723fcec3b5ebfd3b59e3
JulioCesarLB <julio.lifeintech@gmail.com>
2022-04-20 16:56:19 -0300

    Operators

--- src/fiap/tokio/logica/aula/Aula06.java
+++ src/fiap/tokio/logica/aula/Aula06.java
@@ -4,7 +4,33 @@ public class Aula06 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world, test");
+		
+		//Operadores relacionais
+		int num1=90, num2=78;
+		boolean test = num1>num2;
+		System.out.println(test);
+		
+		boolean test2 = num1<num2;
+		System.out.println(test2);
+		
+		boolean test3 = num1!=num2;
+		System.out.println(test3);
+		
+		boolean test4 = num1==num2;
+		System.out.println(test4);
+		
+		// Operadores lógicos 
+		
+		boolean verdadeiro = true, falso = false;
+		
+		boolean AND = verdadeiro && falso;
+		System.out.println(AND);
+		
+		boolean OR = verdadeiro || falso;
+		System.out.println(OR);
+		
+		boolean NOT = !verdadeiro;
+		System.out.println(NOT);
 	}
 
 }
