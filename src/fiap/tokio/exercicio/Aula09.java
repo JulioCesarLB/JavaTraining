d92975159f02893458114300baadc9a74a3cbaa7
JulioCesarLB <julio.lifeintech@gmail.com>
2022-05-02 20:37:56 -0300

    ex 04

--- src/fiap/tokio/exercicio/Aula09.java
+++ src/fiap/tokio/exercicio/Aula09.java
@@ -31,9 +31,29 @@ public class Aula09 {
 	}
 	
 	public static String ex4() {
+		int menor=0,igual=0, maior=0, alunos = Integer.parseInt(JOptionPane.showInputDialog(null,"Quantos alunos tem na sua sala"));
+		double media=0;
+		double[] notas = new double[alunos];
 		
+		for(int i=0; i<alunos; i++) {
+			notas[i]= Integer.parseInt(JOptionPane.showInputDialog(null,"Digite a nota do aluno "+(i+1)));
+			media += notas[i];
+		}
+		media/=alunos; 
 		
-		return null;
+		for(double i:notas) {
+			if(media>i) {
+				menor+=1;
+			}else if(media<i) {
+				maior+=1;
+			}else {
+				igual+=1;
+			}
+		}
+		return "Média: "+media+
+				"\n Notas superiores: "+maior+
+				"\n Notas inferiores: "+menor+
+				"\n Notas iguais: "+igual;
 	}
 
 }
